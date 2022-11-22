c1b766b7e361f009e0515135d7ae8a84b198c129
CrissIHT <115413143+CrissIHT@users.noreply.github.com>
2022-11-22 11:57:05 -0300

    Update Tablero.java

--- src/Clases/Tablero.java
+++ src/Clases/Tablero.java
@@ -1,7 +1,15 @@
 package Clases;
 
 public class Tablero {
-    public Tablero(){
-        
+    private int xpos, ypos;
+    public Tablero(int x, int y){
+        xpos=x;y=ypos;
+    }
+    
+    public void paint(Graphics g){
+        g.setColor(Color.WHITE);
+        g.fillRect(xpos, ypos+150, 500, 500);
+        g.setColor(Color.ORANGE);
+        g.drawRect(xpos, ypos+150, 500, 500);
     }
 }
